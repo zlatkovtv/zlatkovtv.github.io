7c5a2b87be16f8c2544b8932ec4392807de03643
Garrett <zlatkovtv@gmail.com>
2019-08-17 22:43:26 -0700

    first commit

---
+++ precache-manifest.3be561c0e746ac450ecc95fd40bd1cbb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e49aadaae1c08c4530176df6aeda2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04c3f87487e92923790b",
+    "url": "/static/css/2.06565635.chunk.css"
+  },
+  {
+    "revision": "a4f3983ffde822cbe82c",
+    "url": "/static/css/main.cbec4aa7.chunk.css"
+  },
+  {
+    "revision": "04c3f87487e92923790b",
+    "url": "/static/js/2.28be4014.chunk.js"
+  },
+  {
+    "revision": "a4f3983ffde822cbe82c",
+    "url": "/static/js/main.11343766.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b5cae150ec8c9261e6c054c26d003a90",
+    "url": "/static/media/konstantin_zlatkov_cv_2019.b5cae150.pdf"
+  },
+  {
+    "revision": "98f308e708ca57df1350f091f3173d82",
+    "url": "/static/media/profile-image.98f308e7.jpg"
+  }
+]);
